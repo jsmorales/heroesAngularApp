6a960e813de372658f83c2d49c5db00035ca75bc
jsmorales <johan.morales@saiavh.com>
2019-02-20 15:43:06 -0500

    Ajustes finales.

--- src/app/Components/Shared/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/Components/Shared/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -9,12 +9,15 @@ import {Router} from '@angular/router';
 export class HeroeTarjetaComponent implements OnInit {
   @Input() heroe: any = []; // este decorador toma la variable desde afuera enviada en []
   @Input() index: number;
-  constructor(private router: Router) { }
-
+  @Output() heroeSeleccionado: EventEmitter<number>; // ocupa Output y EventEmmiter, <number> especifica que emite un numero
+  constructor(private router: Router) {
+    this.heroeSeleccionado = new EventEmitter(); // se inicializa el emisor del evento
+  }
   ngOnInit() {
   }
   verHeroe() {
-    this.router.navigate(['/heroe', this.index ]);
+    // this.router.navigate(['/heroe', this.index ]);
+    this.heroeSeleccionado.emit(this.index); // se emite el evento enviando el number que se especifica en la declaracion
   }
 
 }
